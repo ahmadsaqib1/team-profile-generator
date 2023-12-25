2af0d16edae7416d3e30808a2d6b3760647de9b8
ahmadsaqib1 <ahmadsaqib1@gmail.com>
2023-12-25 17:19:20 +0500

    install inquirer package

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
-
+console.log("hello");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
-
